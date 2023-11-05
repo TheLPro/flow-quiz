fc21abfa20fe828bf38870a1c447765d90ab8b6c
Leon <leon.potocki@gmail.com>
2023-11-05 15:22:26 +0000

    hello

--- index.js
+++ index.js
@@ -1,1 +1,73 @@
-var data = '';
+var data = `{
+    "s1": {
+      "q1": "null",
+      "q2": "null",
+      "q3": "null"
+    },
+    "s2": {
+      "q1": "null",
+      "q2": "null",
+      "q3": "null"
+    },
+    "s3": {
+      "q1": "null",
+      "q2": "null",
+      "q3": "null"
+    },
+    "s4": {
+      "q1": "null",
+      "q2": "null",
+      "q3": "null"
+    },
+    "s5": {
+      "q1": "null",
+      "q2": "null",
+      "q3": "null"
+    },
+    "s6": {
+      "q1": "null",
+      "q2": "null",
+      "q3": "null"
+    },
+    "s7": {
+      "q1": "null",
+      "q2": "null",
+      "q3": "null"
+    },
+    "s8": {
+      "q1": "null",
+      "q2": "null",
+      "q3": "null"
+    },
+    "s9": {
+      "q1": "null",
+      "q2": "null",
+      "q3": "null"
+    },
+    "s10": {
+      "q1": "null",
+      "q2": "null",
+      "q3": "null"
+    },
+    "s11": {
+      "q1": "null",
+      "q2": "null",
+      "q3": "null"
+    },
+    "s12": {
+      "q1": "null",
+      "q2": "null",
+      "q3": "null"
+    }
+  }`;
+
+var obj = JSON.parse(data);
+var scores = obj;
+
+function addData(score, s, q) {
+    obj[s][q] = score;
+}
+if (scores.s12.q3 != "null") {
+    console.log("test complete");
+}
+
