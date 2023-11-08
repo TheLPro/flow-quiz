8b0da8101232d97641247a897763def09b6e6a32
Leon <leon.potocki@gmail.com>
2023-11-08 07:50:14 +0000

    a

--- scripts/index.js
+++ scripts/index.js
@@ -1,72 +1,76 @@
 var data = `{
     "s1": {
-      "q1": "null",
-      "q2": "null",
-      "q3": "null"
+      "q1": 0,
+      "q2": 0,
+      "q3": 0
     },
     "s2": {
-      "q1": "null",
-      "q2": "null",
-      "q3": "null"
+      "q1": 0,
+      "q2": 0,
+      "q3": 0
     },
     "s3": {
-      "q1": "null",
-      "q2": "null",
-      "q3": "null"
+      "q1": 0,
+      "q2": 0,
+      "q3": 0
     },
     "s4": {
-      "q1": "null",
-      "q2": "null",
-      "q3": "null"
+      "q1": 0,
+      "q2": 0,
+      "q3": 0
     },
     "s5": {
-      "q1": "null",
-      "q2": "null",
-      "q3": "null"
+      "q1": 0,
+      "q2": 0,
+      "q3": 0
     },
     "s6": {
-      "q1": "null",
-      "q2": "null",
-      "q3": "null"
+      "q1": 0,
+      "q2": 0,
+      "q3": 0
     },
     "s7": {
-      "q1": "null",
-      "q2": "null",
-      "q3": "null"
+      "q1": 0,
+      "q2": 0,
+      "q3": 0
     },
     "s8": {
-      "q1": "null",
-      "q2": "null",
-      "q3": "null"
+      "q1": 0,
+      "q2": 0,
+      "q3": 0
     },
     "s9": {
-      "q1": "null",
-      "q2": "null",
-      "q3": "null"
+      "q1": 0,
+      "q2": 0,
+      "q3": 0
     },
     "s10": {
-      "q1": "null",
-      "q2": "null",
-      "q3": "null"
+      "q1": 0,
+      "q2": 0,
+      "q3": 0
     },
     "s11": {
-      "q1": "null",
-      "q2": "null",
-      "q3": "null"
+      "q1": 0,
+      "q2": 0,
+      "q3": 0
     },
     "s12": {
-      "q1": "null",
-      "q2": "null",
-      "q3": "null"
+      "q1": 0,
+      "q2": 0,
+      "q3": 0
     }
   }`;
 
+var parced = JSON.parse(data);
+localStorage.setItem('data', data);
 var obj = JSON.parse(data);
 var scores = obj;
 
 function addData(score, s, q) {
   obj[s][q] = score;
 }
-if (scores.s12.q3 != 'null') {
-  console.log('test complete');
+for (let i = 1; i < 13; i++) {
+  var sel = `s${i}`;
+  var score = obj[sel]['q1'] + obj[sel]['q2'] + obj[sel]['q3'];
+  document.querySelector(`#${sel}`).innerHTML = `${score}/3`;
 }
