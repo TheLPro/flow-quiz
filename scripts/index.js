a0685a2fd04b84eebe28f967530b2af6fecb4bbf
Gammert54 <96523965+Gammert54@users.noreply.github.com>
2023-11-11 17:16:51 +0100

    i did it! We have a baby

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
-if (window.location.href.includes("index.html")) {
-var data = `{
+if (window.location.href.includes('index')) {
+  var data = `{
     "s1": {
       "q1": 1,
       "q2": 1,
@@ -78,23 +78,35 @@ Hello there sir, how could I help you today??????????????????????\tHello there s
 Hello there sir, how could I help you today????????????????????????\tHello there sir, how could I help you today?????????????????????????\tHello there sir, how could I help you today?????????????????????????
 Hello there sir, how could I help you today??????????????????????????\tHello there sir, how could I help you today???????????????????????????\tHello there sir, how could I help you today???????????????????????????
 `;
-var stageTitles = ["Faza 1", "Faza 2", "Faza 3", "Faza 4", "Faza 5", "Faza 6", "Faza 7", "Faza 8", "Faza 9", "Faza 10", "Faza 11", "Faza 12"];
+var stageTitles = [
+  'Faza 1',
+  'Faza 2',
+  'Faza 3',
+  'Faza 4',
+  'Faza 5',
+  'Faza 6',
+  'Faza 7',
+  'Faza 8',
+  'Faza 9',
+  'Faza 10',
+  'Faza 11',
+  'Faza 12'
+];
 var parced = JSON.parse(data);
 localStorage.setItem('data', data);
 var obj = JSON.parse(data);
 var scores = obj;
 var current = 1;
-var nextBtn = document.getElementById("next");
-var prevBtn = document.getElementById("return");
-var title = document.getElementById("title");
-var progress = document.getElementById("progressBar");
-var q1 = document.getElementById("q1");
-var q2 = document.getElementById("q2");
-var q3 = document.getElementById("q3");
-
+var nextBtn = document.getElementById('next');
+var prevBtn = document.getElementById('return');
+var title = document.getElementById('title');
+var progress = document.getElementById('progressBar');
+var q1 = document.getElementById('q1');
+var q2 = document.getElementById('q2');
+var q3 = document.getElementById('q3');
 
 function changeTitle(num) {
-  if (window.location.href.includes("quiz.html")) {
+  if (window.location.href.includes('quiz')) {
     document.title = `Faza ${num} | Test Wypalenia`;
     title.innerHTML = `Faza ${num} - ${stageTitles[num - 1]}`;
     progress.value = num;
@@ -102,41 +114,41 @@ function changeTitle(num) {
 }
 changeTitle(current);
 function next() {
-    if (current < 12) {
-        current++;
-        changeTitle(current);
-        setQuestion(current);
-        resetChekcks();
-    } else {
-        window.location.href = "done.html";
-    }
+  if (current < 12) {
+    current++;
+    changeTitle(current);
+    setQuestion(current);
+    resetChekcks();
+  } else {
+    window.location.href = 'done';
+  }
 }
 function back() {
-    if (current > 1) {
-        current--;
-        changeTitle(current);
-        setQuestion(current);
-        resetChekcks();
-    } else {
-        window.location.href = "index.html";
-    }
+  if (current > 1) {
+    current--;
+    changeTitle(current);
+    setQuestion(current);
+    resetChekcks();
+  } else {
+    window.location.href = 'index';
+  }
 }
 
 if (nextBtn && prevBtn) {
-  nextBtn.addEventListener("click", next);
-  prevBtn.addEventListener("click", back);
+  nextBtn.addEventListener('click', next);
+  prevBtn.addEventListener('click', back);
 }
 
 function addData(score, s, q) {
   obj[s][q] = score;
   localStorage.setItem('data', JSON.stringify(obj));
-  console.log("Added data: " + score + " to " + s + " " + q);
+  console.log('Added data: ' + score + ' to ' + s + ' ' + q);
 }
 for (let i = 1; i < 13; i++) {
   var sel = `s${i}`;
   var score = obj[sel]['q1'] + obj[sel]['q2'] + obj[sel]['q3'];
   if (document.querySelector(`#${sel}`)) {
-    document.querySelector(`#${sel}`).textContent = score + "/3";
+    document.querySelector(`#${sel}`).textContent = score + '/3';
   }
 }
 function tsvParse() {
@@ -152,39 +164,38 @@ function tsvParse() {
 function setQuestion(section) {
   section = section - 1;
   if (q1 && q2 && q3) {
-  q1.innerHTML = tsvParse()[section][0];
-  q2.innerHTML = tsvParse()[section][1];
-  q3.innerHTML = tsvParse()[section][2];
+    q1.innerHTML = tsvParse()[section][0];
+    q2.innerHTML = tsvParse()[section][1];
+    q3.innerHTML = tsvParse()[section][2];
   }
 }
 
 setQuestion(current);
-if (window.location.href.includes("quiz")) {
-  document.querySelector("#answer_1_1").addEventListener("click", function() {
-    addData(1, `s${current}`, "q1");
+if (window.location.href.includes('quiz')) {
+  document.querySelector('#answer_1_1').addEventListener('click', function () {
+    addData(1, `s${current}`, 'q1');
   });
-  document.querySelector("#answer_1_0").addEventListener("click", function() {
-    addData(0, `s${current}`, "q1");
+  document.querySelector('#answer_1_0').addEventListener('click', function () {
+    addData(0, `s${current}`, 'q1');
   });
-  document.querySelector("#answer_2_1").addEventListener("click", function() {
-    addData(1, `s${current}`, "q2");
+  document.querySelector('#answer_2_1').addEventListener('click', function () {
+    addData(1, `s${current}`, 'q2');
   });
-  document.querySelector("#answer_2_0").addEventListener("click", function() {
-    addData(0, `s${current}`, "q2");
+  document.querySelector('#answer_2_0').addEventListener('click', function () {
+    addData(0, `s${current}`, 'q2');
   });
-  document.querySelector("#answer_3_1").addEventListener("click", function() {
-    addData(1, `s${current}`, "q3");
+  document.querySelector('#answer_3_1').addEventListener('click', function () {
+    addData(1, `s${current}`, 'q3');
   });
-  document.querySelector("#answer_3_0").addEventListener("click", function() {
-    addData(0, `s${current}`, "q3");
+  document.querySelector('#answer_3_0').addEventListener('click', function () {
+    addData(0, `s${current}`, 'q3');
   });
 }
 
-
-function resetChekcks(){
+function resetChekcks() {
   const buttons = document.querySelectorAll("input[type='radio']");
   buttons.forEach((button) => {
-    if(button.value == 'true'){
+    if (button.value == 'true') {
       button.checked = true;
     } else {
       button.checked = false;
@@ -192,7 +203,11 @@ function resetChekcks(){
   });
 }
 
-document.addEventListener("load", function() {
+document.addEventListener('load', function () {
   resetChekcks();
-  console.log("Loaded");
+  console.log('Loaded');
+});
+
+document.querySelector('.special').addEventListener('click', function () {
+  window.location.href = 'quiz';
 });
